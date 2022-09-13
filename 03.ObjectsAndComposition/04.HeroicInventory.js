ad3102b5a55f7b9642c1f2c1b1126a37662ade9f
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-09-13 14:41:11 +0300

    Solved Problem 4.

---
+++ 03.ObjectsAndComposition/04.HeroicInventory.js
@@ -0,0 +1,14 @@
+function solve(input){
+let res=[];
+for (const iterator of input) {
+    let[name,level,items]=iterator.split(' / ');
+    level=Number(level);
+    items =items?items.split(', '):[];
+res.push({name,level,items});
+}
+console.log(JSON.stringify(res));
+}
+
+solve(['Isacc / 25 / Apple, GravityGun',
+'Derek / 12 / BarrelVest, DestructionSword',
+'Hes / 1 / Desolator, Sentinel, Antara']);
