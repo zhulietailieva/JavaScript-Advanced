069efc4fa33eeee7fa9e56a65eb4b62a6513928f
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-09-17 23:42:27 +0300

    Solved Problem 5.

--- 04.DOMIntroduction/05. Table - Search Engine/solution.js
+++ 04.DOMIntroduction/05. Table - Search Engine/solution.js
@@ -2,7 +2,20 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
-
+      var textToSearch=document.getElementById('searchField').value;
+     var tableRowsElements=Array.from(document.getElementsByTagName('tr')).slice(2);
+     
+   console.log(tableRowsElements);
+for (const rowElement of tableRowsElements) {
+   var rowElementArray=rowElement.textContent.split('\n');
+   console.log(rowElementArray);
+   if(rowElement.textContent.includes(textToSearch)){
+      rowElement.className='select';
+   }
+   else{
+      rowElement.classList.remove('select');
+   }
+}
+document.getElementById('searchField').value='';
    }
 }
