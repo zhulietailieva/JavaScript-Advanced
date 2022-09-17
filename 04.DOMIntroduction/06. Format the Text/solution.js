fe1adb4a94b77c3f0b3b7afb549bb33d41df746a
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-09-18 01:22:25 +0300

    Completed Exercise 6.

--- 04.DOMIntroduction/06. Format the Text/solution.js
+++ 04.DOMIntroduction/06. Format the Text/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO
+  let text = document.getElementById('input').value;
+  let sentances = text.split('.');
+  let count = 0;
+  let res = '';
+  for(let sentence of sentances){
+      if (sentence.length < 1){
+        continue;
+      }
+      count += 1;
+      if (count <= 3){
+        res += `${sentence}.`;
+      }
+      else{
+        document.getElementById('output').innerHTML += `<p>${res}</p>`;
+        count = 1;
+        res = '';
+        res = `${sentence}.`;
+      }
+  }
+  document.getElementById('output').innerHTML += `<p>${res}</p>`;
 }
