9cc63d0142c53c009f433d73d8df82fff53e9810
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-09-17 22:29:38 +0300

    Completed Exercise 3.

--- 04.DOMIntroduction/03. Accordion/accordion.js
+++ 04.DOMIntroduction/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
-function toggle() {
-    console.log('TODO:...');
+function toggle() {   
+    console.log('here') ; 
+    var buttonEl=document.getElementsByClassName('button')[0];
+    
+    if(buttonEl.textContent=='More'){
+        document.getElementsByClassName('button')[0].textContent='Less';
+        document.getElementById('extra').style.display='block';
+    }
+   else if(buttonEl.textContent=='Less'){
+    document.getElementsByClassName('button')[0].textContent='More';
+        document.getElementById('extra').style.display='none';
+    }
 }
