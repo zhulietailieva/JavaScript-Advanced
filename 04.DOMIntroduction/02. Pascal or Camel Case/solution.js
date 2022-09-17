31d513afeea48158f7b3d8bf3aff1b0019c41f48
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-09-17 21:59:33 +0300

    Completed Exercise 2.

--- 04.DOMIntroduction/02. Pascal or Camel Case/solution.js
+++ 04.DOMIntroduction/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,30 @@
 function solve() {
-  //TODO...
+  var typeInput=document.getElementById('naming-convention').value;
+  var input=document.getElementById('text').value;
+  let toPascalCase=(input)=>{
+  return input
+  .toLowerCase()
+  .split(' ')
+  .map(x=>x.toUpperCase()[0]+x.substring(1,x.length))
+  .join('');
+  }
+  let toCamelCase=(input)=>{
+    return input
+    .toLowerCase()
+    .split(' ')
+    .map((x,pos)=>pos==0?x:x.toUpperCase()[0]+x.substring(1,x.length))
+    .join('');
+  }
+  var res='';
+if(typeInput=='Camel Case'){
+res=toCamelCase(input);
+}
+else if(typeInput=='Pascal Case'){
+res=toPascalCase(input);
+}
+else{
+res='Error!';
+}
+console.log(res);
+document.getElementById('result').textContent=res;
 }
