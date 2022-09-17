ffc9b9f30f1b41f1a1ebdd6c6bad6d9b6e38cc01
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-09-17 22:59:31 +0300

    Completed Task 4.

--- 04.DOMIntroduction/04. Search in List/search.js
+++ 04.DOMIntroduction/04. Search in List/search.js
@@ -1,3 +1,18 @@
 function search() {
-   // TODO
+   var townsElements=Array.from(document.getElementById('towns').children);
+   var textToSearch=document.getElementById('searchText').value;
+   console.log(textToSearch);
+   var matches=0;
+   for (const town of townsElements) {
+      if(town.textContent.includes(textToSearch)){
+         town.style.fontWeight='bold';
+         town.style.textDecoration='underline';
+         matches++;
+      }
+      else{
+         town.style.fontWeight='normal';
+         town.style.textDecoration='none';
+      }
+   }
+   document.getElementById('result').textContent=`${matches} matches found`;  
 }
