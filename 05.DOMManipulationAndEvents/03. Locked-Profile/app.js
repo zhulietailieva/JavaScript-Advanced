c91cf9af3e57ecfd6f8b85dbd3e2a0b7efdfde11
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-09-29 22:20:15 +0300

    Problem 3

--- 05.DOMManipulationAndEvents/03. Locked-Profile/app.js
+++ 05.DOMManipulationAndEvents/03. Locked-Profile/app.js
@@ -1,3 +1,29 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttonsElements=document.getElementsByTagName('button');
+    for(let i=0;i<buttonsElements.length;i++){
+        let button=buttonsElements[i];
+        let profileElement=button.parentElement;
+        let hiddenElement=document.getElementById(`user${i+1}HiddenFields`);
+        let isLockedElement=profileElement.querySelector('input[value="lock"]');
+
+        button.addEventListener('click',()=>{
+            if(!isLockedElement.checked&&buttonsElements[i].textContent=="Show more"){
+                hiddenElement.style.display='block';
+                button.textContent="Hide it";
+            }
+            else if(!isLockedElement.checked&&buttonsElements[i].textContent=="Hide it"){
+                hiddenElement.style.display='';
+                button.textContent="Show more";
+            }
+        
+
+            
+        })
+    }
+    
+
+    function unlcok(){
+
+        
+    }
 }
