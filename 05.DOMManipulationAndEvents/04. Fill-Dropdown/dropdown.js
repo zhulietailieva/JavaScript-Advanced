e531e3c9e4b2900c9e3b0555741175ecec8c01e1
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-09-29 22:50:33 +0300

    Problem 4

--- 05.DOMManipulationAndEvents/04. Fill-Dropdown/dropdown.js
+++ 05.DOMManipulationAndEvents/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+let newItemText=document.getElementById("newItemText").value;
+let newItemValue=document.getElementById("newItemValue").value;
+let menuElement=document.getElementById('menu');
+let optionElement =document.createElement('option');
+optionElement.textContent=newItemText;
+optionElement.value=newItemValue;
+menuElement.appendChild(optionElement);
+document.getElementById("newItemText").value='';
+document.getElementById("newItemValue").value='';
 }
