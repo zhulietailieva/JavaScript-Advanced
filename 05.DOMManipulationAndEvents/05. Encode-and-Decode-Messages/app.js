5a3c5839c21ca26a7e8f6125555128dcaa0ad6af
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-09-30 20:40:29 +0300

    Started working on Problem 5.

--- 05.DOMManipulationAndEvents/05. Encode-and-Decode-Messages/app.js
+++ 05.DOMManipulationAndEvents/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,22 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let buttonElements=document.getElementsByTagName('button');
+    for(let i=0;i<buttonElements.length;i++){
+        let btn=buttonElements[i];
+        btn.addEventListener('click',()=>{
+            if(i==0){
+                let messageInp=document.getElementsByTagName('textarea')[0].value;
+                let codedMessage=encode(message);
+                encode('a');
+            }
+        })
+    }
+    function encode(message){
+        let symbols=Array.from(message);
+        let res='';
+        for (const symbol of symbols) {
+            let currentSymbol=Number(symbol);
+            console.log(currentSymbol);
+        }
+    }
+    
 }
---
+++ 05.DOMManipulationAndEvents/05. Encode-and-Decode-Messages/test.js
@@ -0,0 +1,11 @@
+function solve(message){
+   
+        let symbols=Array.from(message);
+        let res='';
+        for (const symbol of symbols) {
+            let currentSymbol=String.charCodeAt
+            console.log(currentSymbol);
+        }
+    
+}
+solve("a");
