cd9033e36590d11b8b304f359207c6a303f07f80
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-10-06 17:28:33 +0300

    Problem 7.

--- 06.AdvancedFunctions/07. Simple Calculator/app.js
+++ 06.AdvancedFunctions/07. Simple Calculator/app.js
@@ -1,7 +1,29 @@
 function calculator() {
-    // TODO:
+let num1;
+let num2;
+let res;
+function init(selector1, selector2, resultSelector){
+    num1=document.querySelector(selector1);
+   num2=document.querySelector(selector2);
+   res=document.querySelector(resultSelector); 
+}   
+function add(){
+    let n1=Number(num1.value);
+    let n2=Number(num2.value);
+    res.value=n1+n2;
 }
-
-
-
+function subtract(){
+    let n1=Number(num1.value);
+    let n2=Number(num2.value);
+    res.value=n1-n2;
+}
+    let object={
+        init,
+        add,
+        subtract
+    };
+    return object;
+}
+let calculate = calculator();
+calculate.init('#num1', '#num2', '#result');
 
