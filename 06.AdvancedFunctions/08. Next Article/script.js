58084dc3307fd91a4d8ddf0b92c839e3371181ce
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-10-06 17:49:28 +0300

    Problem 8.

--- 06.AdvancedFunctions/08. Next Article/script.js
+++ 06.AdvancedFunctions/08. Next Article/script.js
@@ -1,3 +1,13 @@
 function getArticleGenerator(articles) {
-    // TODO
+    console.log(articles.length);
+let elementToDisplayOn=document.getElementById('content');
+function showNext(){
+    if(articles.length>0){
+       let currentArticle=articles.shift();
+       let article=document.createElement('article');
+       article.textContent=currentArticle;
+        elementToDisplayOn.appendChild(article);
+    }
+}
+return showNext;
 }
