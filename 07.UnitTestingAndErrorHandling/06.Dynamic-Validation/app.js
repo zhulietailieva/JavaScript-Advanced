87e43381362bab3739be2485172e0f0f39f8938e
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-10-08 16:38:40 +0300

    Started working on Problem 7.

--- 07.UnitTestingAndErrorHandling/06.Dynamic-Validation/app.js
+++ 07.UnitTestingAndErrorHandling/06.Dynamic-Validation/app.js
@@ -1,6 +1,6 @@
 function validate() {
     let validEmailPattern=new RegExp('[a-z]+@{1}[a-z]+.{1}[a-z]+');
-    let inputElement=document.getElementById('id');
+   // let inputElement=document.getElementById('id');
     document.addEventListener('change',(e)=>{
         if(!validEmailPattern.test(e.target.value)){
             e.target.classList.add('error');
--- 07.UnitTestingAndErrorHandling/07.Form-Validation/formValidation.js
+++ 07.UnitTestingAndErrorHandling/07.Form-Validation/formValidation.js
@@ -1,3 +1,39 @@
+let usernamePattern=new RegExp('^[A-Za-z0-9]{3,20}$');
+    let passwordPattern=new RegExp('^[A-Za-z0-9_]{5,15}$');
+    let emailPattern=new RegExp('[a-z]+@{1}[a-z]+.{1}[a-z]+');
+    
+//get the elements
+let usernameElement=document.getElementById('username');
+let emailElement=document.getElementById('email');
+let passwordElement=document.getElementById('password');
+let confirmPasswordElement=document.getElementById('confirm-password');
+let companyElement=document.getElementById('company');
+let companyElementField=document.getElementById('companyInfo');
+let companyNumberElement=document.getElementById('companyNumber');
+let buttonElement=document.getElementById('submit');
 function validate() {
-    // TODO
+//username validation
+if(!usernamePattern.test(usernameElement.value)){
+    usernameElement.style.borderColor='red';
+    console.log('not valid');
+}
+else{
+    usernameElement.style.borderColor='none';
+    console.log('valid');
+}
+//email validation
+
+
+companyElement.addEventListener('change',(e)=>{
+    console.log('here');
+    if(e.target.checked){
+        console.log('changed to true')
+        companyElementField.style.display='block';
+    } 
+    else{
+        console.log('changed to false')
+        companyElementField.style.display='none';
+    }
+})
+    
 }
