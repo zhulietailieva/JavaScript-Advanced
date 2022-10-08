edc12a614c0cd2781f74badf8cd676aa8807a40e
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-10-08 15:51:47 +0300

    Problem 6.

--- 07.UnitTestingAndErrorHandling/06.Dynamic-Validation/app.js
+++ 07.UnitTestingAndErrorHandling/06.Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    // TODO
+    let validEmailPattern=new RegExp('[a-z]+@{1}[a-z]+.{1}[a-z]+');
+    let inputElement=document.getElementById('id');
+    document.addEventListener('change',(e)=>{
+        if(!validEmailPattern.test(e.target.value)){
+            e.target.classList.add('error');
+        }
+        else{
+            e.target.classList.remove('error');
+        }
+    })
 }
